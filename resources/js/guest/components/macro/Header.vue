9da3db3d58b863221ff36ddea7f153ddbbf91821
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-04-15 17:15:46 +0200

    header and searchbar base complete and responsive

--- resources/js/guest/components/macro/Header.vue
+++ resources/js/guest/components/macro/Header.vue
@@ -29,19 +29,27 @@
     </nav>
 
     <div class="main-searchbar d-flex justify-content-center align-item-center">
-        <form>
-            <input type="text" name="search" id="search" placeholder="Search">
-            <select name="services" id="services">
-                <option value="" hidden aria-hidden>Services</option>
-                <option value="service1">service-1</option>
-                <option value="service2">service-2</option>
-                <option value="service3">service-3</option>
-                <option value="service4">service-4</option>
-            </select>
-            <label for="beds">Beds:</label>
-            <input type="number" name="beds" id="beds" value="1" min="1" max="10">
+        <form class="d-flex justify-content-center align-items-center">
+            <input type="text" name="text" id="text" placeholder="search">
 
-            <input type="submit" value="Submit">
+            <div class="multiselect justify-content-center align-items-center d-none d-sm-flex" id="multiselect">
+                <div class="services" @click="services()">Services &#x2193;</div>
+                <div class="multiselect-options hidden" id="multiselectOptions">
+                    <div>
+                        <input type="checkbox" name="checkbox1" id="">
+                        <label for="checkbox1">1</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="checkbox2" id="">
+                        <label for="checkbox2">2</label>
+                    </div>
+                </div>
+            </div>
+
+            <label for="beds" class="d-none d-sm-block">Beds:</label>
+            <input type="number" name="beds" id="beds" value="1" min="1" max="10" class="d-none d-sm-block">
+
+            <input type="submit" value="Submit" id="submit">
         </form>
     </div>
 </header>
@@ -50,6 +58,24 @@
 <script>
 export default {
     name: 'Header',
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        services(){
+            //funzione per far sparire ed apparire le opzioni checkbox
+
+            let multiselectOptions = document.getElementById("multiselectOptions");
+
+            if(multiselectOptions.classList.contains("hidden")){
+                multiselectOptions.classList.remove("hidden");
+            }else{
+                multiselectOptions.classList.add("hidden");
+            }
+        }
+    }
 }
 </script>
 
@@ -64,17 +90,6 @@ header{
     position: relative;
     margin-bottom: 3rem;
 }
-
-.main-searchbar{
-    width: 100%;
-    position: absolute;
-    bottom: 0;
-    transform: translateY(50%);
-
-    & form{
-    }
-}
-
 .wrapper{
     position: absolute;
     width: 100%;
@@ -109,6 +124,74 @@ header{
         filter: brightness(1.2);
         box-shadow: 0px 2px 6px 1px rgba(0,0,0,.3);
     }
+}
+
+
+.main-searchbar{
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    transform: translateY(50%);
+
+    & form{
+        background-color: $primary-red;
+        width: fit-content;
+        padding: 5px;
+        height: 80px;
+        border-radius: 3rem;
+        overflow: hidden;
+        
+        & > *{
+            border: none;
+            background-color: transparent;
+            color: white;
+        }
+
+        & input#text{ 
+            width: 100px;
+            padding-left: 10px;
+        }
+        & .multiselect{
+            width: 100px;
+            margin: 0 20px;
+        }
+        & input#beds{
+            width: 50px;
+            margin: 0 10px;
+        }
+        & input#submit{
+            background-color: white;
+            color: black;
+            height: 4rem;
+            width: 4rem;
+            border-radius:50% ;
+        }
+    }
+}
+
+label{
+    margin: 0;
+}
+::placeholder{
+    color: white;
+}
+
+.multiselect-options{
+    position: absolute;
+    bottom: -50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100px;
+    background-color: white;
+    border: 1px solid gray;
+    padding: 10px;
+    border-radius: 1rem;
+    color: $primary-red;
+}
+.multiselect-options.hidden{
+    display: none;
 }
 
 </style>
