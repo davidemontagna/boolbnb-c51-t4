d99712d093c209d3f8e6d07c312ee2a606940fa4
Masadavide <davide.masadm@gmail.com>
2022-04-15 10:14:37 +0200

    rimosso bootstrap e rimesso axios

--- resources/js/guest/guest.js
+++ resources/js/guest/guest.js
@@ -1,4 +1,7 @@
-require('./bootstrap');
+/* require('./bootstrap'); */
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Vue = require('vue');
 
