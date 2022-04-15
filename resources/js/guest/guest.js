6d731dbdeeb34e3b0fa8de73b5bb03e1a4e14e16
Masadavide <davide.masadm@gmail.com>
2022-04-15 10:20:33 +0200

    aggiunto bootstrap

--- resources/js/guest/guest.js
+++ resources/js/guest/guest.js
@@ -1,7 +1,4 @@
-/* require('./bootstrap'); */
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+require('../bootstrap');
 
 window.Vue = require('vue');
 
