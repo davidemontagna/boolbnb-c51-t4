1801afc7a2608d44e76742fa54228beeac0dc74d
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-04-15 11:42:38 +0200

    footer base, no social icons

--- resources/js/guest/components/macro/Footer.vue
+++ resources/js/guest/components/macro/Footer.vue
@@ -1,6 +1,13 @@
 <template>
 <footer>
-    <h1>Footer</h1>
+    <div class="footer-container">
+        <div class="footer-left">© 2022 BoolBnb, Inc.</div>
+        <div class="footer-socials">
+            <div>F</div>
+            <div>I</div>
+            <div>T</div>
+        </div>
+    </div>
 </footer>
 </template>
 
@@ -11,5 +18,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../../../sass/global.scss';
 
+footer{
+    margin-top: 1rem;
+    background: linear-gradient(0deg, rgba(144, 144, 144, 0.5) 0%, rgba(255,255,255,0) 60%);
+}
+    .footer-container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 3rem;
+        width: calc(100% - 2rem);
+        padding: 0 1rem;
+
+        .footer-socials{
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+
+            & > *{
+                padding: 0 .3rem;
+            }
+        }
+    }
 </style>
