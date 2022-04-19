58e8c22ba36cb84189fcf208f33f9b4943df3673
Alessio-Tomei <alessiotomei22@gmail.com>
2022-04-19 10:58:51 +0200

    Mod. migration seeder model manca controller views

--- app/Apartment.php
+++ app/Apartment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    protected $fillable = ['user_id', 'title', 'num_rooms', 'num_beds', 'num_bath', 'square_footage', 'preview', 'visible', 'description'];
+    protected $fillable = ['user_id', 'title', 'price', 'num_guest', 'num_rooms', 'num_beds', 'num_bath', 'square_footage', 'preview', 'visible', 'description'];
 
     public function user() {    
         return $this->belongsTo('App\User');
--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -13,30 +13,52 @@ class ApartmentSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $randomData = [];
+
+        for ($i=0; $i < 21; $i++) { 
+            $rooms = rand(1, 10);
+            $beds = ceil(($rooms / 2) + rand(0, 1));
+            $baths = ceil(($rooms / 3) + rand(0, 1));
+
+            $randomData = [
+                'rooms' => $rooms,
+                'beds' => $beds,
+                'baths' => $baths,
+            ];
+        }
+
         for ($i=0; $i < 11; $i++) { 
             $newApartment = new Apartment();
             
             $newApartment->user_id = 1 ;
             $newApartment->title = $faker->words(5, true);
-            $newApartment->num_rooms = rand(1, 10);
-            $newApartment->num_beds = rand(1, 10);
-            $newApartment->num_bath = rand(1, 4);
-            $newApartment->square_footage = rand(20, 200);
+            $newApartment->num_rooms = $randomData[$i]['rooms'];
+            $newApartment->num_beds = $randomData[$i]['beds'];
+            $newApartment->num_bath = $randomData[$i]['baths'];
+            $newApartment->num_guest = $randomData[$i]['beds'];
+            $newApartment->price = 10 + ($randomData[$i]['beds'] * 15);
+            $newApartment->square_footage = ($randomData[$i]['rooms'] * 12) + ($randomData[$i]['baths'] * 4);
             $newApartment->preview = 'https://placeimg.com/640/480/arch';
             $newApartment->visible = rand(0, 1);
             $newApartment->description = $faker->text();           
             $newApartment->save();
         }
 
+        $rooms = rand(1, 10);
+        $beds = ceil(($rooms / 2) + rand(0, 1));
+        $baths = ceil(($rooms / 3) + rand(0, 1));
+
         for ($i=0; $i < 10; $i++) { 
             $newApartment = new Apartment();
             
             $newApartment->user_id = 2 ;
             $newApartment->title = $faker->words(5, true);
-            $newApartment->num_rooms = rand(1, 10);
-            $newApartment->num_beds = rand(1, 10);
-            $newApartment->num_bath = rand(1, 4);
-            $newApartment->square_footage = rand(20, 200);
+            $newApartment->num_rooms = $randomData[$i + 11]['rooms'];
+            $newApartment->num_beds = $randomData[$i + 11 ]['beds'];
+            $newApartment->num_bath = $randomData[$i + 11]['baths'];
+            $newApartment->num_guest = $randomData[$i + 11]['beds'];
+            $newApartment->price = 10 + ($randomData[$i + 11]['beds'] * 15);
+            $newApartment->square_footage = ($randomData[$i + 11]['rooms'] * 12) + ($randomData[$i]['baths'] * 4);
             $newApartment->preview = 'https://placeimg.com/640/480/arch';
             $newApartment->visible = rand(0, 1);
             $newApartment->description = $faker->text();           
