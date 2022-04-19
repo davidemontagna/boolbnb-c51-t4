1f62d53c52b3d19a1aafa1dfd9e46fd39d21f679
Masadavide <davide.masadm@gmail.com>
2022-04-19 12:11:35 +0200

    immagini apartments generata casualmente

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="{{asset("css/globals.css")}}">
-    <link rel="stylesheet" href="{{asset("css/guest.css")}}">
     <title>Document</title>
 </head>
 <body>
