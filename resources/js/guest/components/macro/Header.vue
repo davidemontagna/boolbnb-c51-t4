210b9399706befffd40bfe195b992f7178ebdf12
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-04-21 11:04:05 +0200

    spostata searchbar da header a main

--- resources/js/guest/components/macro/Header.vue
+++ resources/js/guest/components/macro/Header.vue
@@ -27,31 +27,6 @@
             </div>
         </div>
     </nav>
-
-    <div class="main-searchbar d-flex justify-content-center align-item-center">
-        <form class="d-flex justify-content-center align-items-center">
-            <input type="text" name="text" id="text" placeholder="search">
-
-            <div class="multiselect justify-content-center align-items-center d-none d-sm-flex" id="multiselect">
-                <div class="services" @click="services()">Services &#x2193;</div>
-                <div class="multiselect-options hidden" id="multiselectOptions">
-                    <div>
-                        <input type="checkbox" name="checkbox1" id="">
-                        <label for="checkbox1">1</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="checkbox2" id="">
-                        <label for="checkbox2">2</label>
-                    </div>
-                </div>
-            </div>
-
-            <label for="beds" class="d-none d-sm-block">Beds:</label>
-            <input type="number" name="beds" id="beds" value="1" min="1" max="10" class="d-none d-sm-block">
-
-            <input type="submit" value="Submit" id="submit">
-        </form>
-    </div>
 </header>
 </template>
 
@@ -138,74 +113,6 @@ header{
         filter: brightness(1.2);
         box-shadow: 0px 2px 6px 1px rgba(0,0,0,.3);
     }
-}
-
-
-.main-searchbar{
-    width: 100%;
-    position: absolute;
-    bottom: 0;
-    transform: translateY(50%);
-
-    & form{
-        background-color: $primary-red;
-        width: fit-content;
-        padding: 5px;
-        height: 80px;
-        border-radius: 3rem;
-        overflow: hidden;
-        
-        & > *{
-            border: none;
-            background-color: transparent;
-            color: white;
-        }
-
-        & input#text{ 
-            width: 100px;
-            padding-left: 10px;
-        }
-        & .multiselect{
-            width: 100px;
-            margin: 0 20px;
-        }
-        & input#beds{
-            width: 50px;
-            margin: 0 10px;
-        }
-        & input#submit{
-            background-color: white;
-            color: black;
-            height: 4rem;
-            width: 4rem;
-            border-radius:50% ;
-        }
-    }
-}
-
-label{
-    margin: 0;
-}
-::placeholder{
-    color: white;
-}
-
-.multiselect-options{
-    position: absolute;
-    bottom: -50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    width: 100px;
-    background-color: white;
-    border: 1px solid gray;
-    padding: 10px;
-    border-radius: 1rem;
-    color: $primary-red;
-}
-.hidden{
-    display: none;
 }
 
 .header-scrolled-bg.scrolled{
--- resources/js/guest/components/macro/Main.vue
+++ resources/js/guest/components/macro/Main.vue
@@ -1,5 +1,29 @@
 <template>
 <main>
+    <div class="main-searchbar d-flex justify-content-center align-item-center">
+        <div class="form d-flex justify-content-center align-items-center">
+            <input type="text" name="text" id="text" placeholder="search">
+
+            <div class="multiselect justify-content-center align-items-center d-none d-sm-flex" id="multiselect">
+                <div class="services" @click="services()">Services &#x2193;</div>
+                <div class="multiselect-options hidden" id="multiselectOptions">
+                    <div>
+                        <input type="checkbox" name="checkbox1" id="">
+                        <label for="checkbox1">1</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="checkbox2" id="">
+                        <label for="checkbox2">2</label>
+                    </div>
+                </div>
+            </div>
+
+            <label for="beds" class="d-none d-sm-block">Beds:</label>
+            <input type="number" name="beds" id="beds" value="1" min="1" max="10" class="d-none d-sm-block">
+
+            <input type="submit" value="Submit" id="submit">
+        </div>
+    </div>
     <router-view :key="$route.path"></router-view>   
 </main>
 </template>
@@ -15,5 +39,72 @@ export default {
 @import '../../../../sass/variables.scss';
 main{
     background-color: $primary-light;
+}
+
+.main-searchbar{
+    width: 100%;
+    position: relative;
+    top: -130px;
+    transform: translateY(50%);
+
+    & .form{
+        background-color: $primary-red;
+        width: fit-content;
+        padding: 5px;
+        height: 80px;
+        border-radius: 3rem;
+        overflow: hidden;
+        
+        & > *{
+            border: none;
+            background-color: transparent;
+            color: white;
+        }
+
+        & input#text{ 
+            width: 100px;
+            padding-left: 10px;
+        }
+        & .multiselect{
+            width: 100px;
+            margin: 0 20px;
+        }
+        & input#beds{
+            width: 50px;
+            margin: 0 10px;
+        }
+        & input#submit{
+            background-color: white;
+            color: black;
+            height: 4rem;
+            width: 4rem;
+            border-radius:50% ;
+        }
+    }
+}
+
+label{
+    margin: 0;
+}
+::placeholder{
+    color: white;
+}
+
+.multiselect-options{
+    position: absolute;
+    bottom: -50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100px;
+    background-color: white;
+    border: 1px solid gray;
+    padding: 10px;
+    border-radius: 1rem;
+    color: $primary-red;
+}
+.hidden{
+    display: none;
 }
 </style>
