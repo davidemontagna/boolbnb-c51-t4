61a5367be11b4e52e7addfd607e52644d9384dc4
Alessio-Tomei <alessiotomei22@gmail.com>
2022-04-21 15:54:50 +0200

    select location in create apartment

--- resources/js/user/LocationApp.vue
+++ resources/js/user/LocationApp.vue
@@ -1,9 +1,15 @@
 <template>
 <div id="location-app">
-  <div class="mb-3">
-    <label for="address" class="form-label">Indirizzo:</label>
-    <input type="text" class="form-control" id="address" name="address" placeholder="Inserisci l'indirizzo da cercare" v-model="addressQuery">
-    <button class="btn btn-primary" @click.prevent="getAddresses">Cerca</button>     
+  <label for="address" class="form-label">Indirizzo:</label>
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" id="address" name="address" @keydown.prevent.enter="getAddresses" placeholder="Inserisci l'indirizzo da cercare" v-model.trim="addressQuery">
+    <div class="input-group-append">
+      <button class="btn btn-outline-secondary" type="button" @click.prevent="getAddresses">Cerca</button>
+    </div>
+    <select class="form-control" v-model="selected">
+      <option value="">Seleziona un indirizzo</option>
+      <option :value="index" v-for="(address, index) in setOption" :key="index">{{address.address.freeformAddress + ', ' + address.address.country}}</option>
+    </select>
   </div>
 </div>
 </template>
@@ -16,25 +22,36 @@ export default {
         return {
             addressesList: [],
             addressQuery: '',
+            oldQuery: '',
             apiKey: 'LmxBM8DrAJjBA1BQPufxlrTGrO4c4Byh',
+            selected: '',
         }
     },
     methods: {
         getAddresses: function() {
-            const apiUrl = 'https://api.tomtom.com/search/2/search/' + this.addressQuery + '.json?minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=' + this.apiKey;
-            axios.get(apiUrl, {transformRequest: (data, headers) => {
-                delete headers.common['X-Requested-With'];
-              } 
-            })
-            .then(apiResponse => {
-                this.addressList = apiResponse.data.results;
-                console.log(this.addressList);
-                })
-            .catch(() => {
-                console.log('error catch api');
-            });
-        }  
+            if (this.addressQuery != '' && this.addressQuery != this.oldQuery) {
+              this.oldQuery = this.addressQuery;
+              const apiUrl = 'https://api.tomtom.com/search/2/search/' + this.addressQuery + '.json?minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=' + this.apiKey;
+              axios.get(apiUrl, {transformRequest: (data, headers) => {
+                  delete headers.common['X-Requested-With'];
+                } 
+              })
+              .then(apiResponse => {
+                  this.addressesList = apiResponse.data.results;
+                  // console.log(this.addressList);
+                  })
+              .catch(() => {
+                  console.log('error catch api');
+              });
+            }
+        },  
     },
+    computed: {
+      setOption() {
+        console.log(this.addressesList);
+        return this.addressesList;
+      }
+    }
 }
 </script>
 
