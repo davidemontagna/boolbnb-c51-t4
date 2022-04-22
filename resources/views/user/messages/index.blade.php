914ba0890bb56dc7a34fd0915f2c288d98bca848
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-04-22 16:23:00 +0200

    messages show e index

--- resources/views/user/messages/index.blade.php
+++ resources/views/user/messages/index.blade.php
@@ -5,49 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Test</title>
+    <title>Messaggi</title>
 </head>
 <body>
-    <div class="container">
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Apartment id</th>
-                <th scope="col">Messaggio</th>
-                <th scope="col">email</th>
-                <th scope="col">nome</th>
-                <th scope="col">visualizzato</th>
-                <th scope="col">risposta</th>
-                <th scope="col">user id</th>
-                <th scope="col">azioni</th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach ($messages as $message)
-                <tr>
-                    <td scope="row">{{$message->id}}</td>
-                    <td>{{$message->apartment_id}}</td>
-                    <td>{{$message->content}}</td>
-                    <td>{{$message->sender_email}}</td>
-                    <td>{{$message->sender_name}}</td>
-                    <td>{{$message->visualized}}</td>
-                    <td>{{$message->answered}}</td>
-                    <td>{{$message->apartment->user_id}}</td>
-                    <td>
-                        <a href="{{route('user.messages.show', $message->id)}}" class="d-inline-block"><button type="button" class="btn btn-secondary">Show</button></a> 
-                        <a href="{{route('user.messages.edit', $message->id)}}" class="d-inline-block"><button type="button" class="btn btn-primary">Edit</button></a>                    
-                        <form action="{{route('user.messages.destroy', $message->id)}}" method="POST">
-                            @csrf
-                            @method("DELETE")
-                            <button type="submit" class="btn btn-danger">Elimina</button>
-                          </form>
-                      </td> 
-                </tr>
-                @endforeach
-            
-            </tbody>
-        </table>
-    </div>
+    
+    <div class="container mt-2" id="messages">
+        <a href="{{route('login')}}" class="d-inline-block mr-2"><button type="button" class="btn btn-danger">back</button></a>                    
+        <div class="row d-flex justify-content-center align-items-center m-4">
+            <h1 class="container">Messaggi da leggere</h1>
+            @foreach ($messages as $message)
+                @if ($message->visualized == 0)
+                <a href="{{route('user.messages.show', $message->id)}}" class="text-decoration-none text-light">
+                    <div class="card col-12 col-md-4 col-xl-3 m-1 messaggio msg-da-leggere {{ ($message->visualized == 0) ? 'text-white bg-dark' : '' }}" style="width: 20rem !important;">
+                        <img src="{{ asset("storage/{$message->apartment->preview}")}}" class="card-img-top mt-3" alt="...">
+                        <div class="card-body">
+                            <p class="card-text d-flex justify-content-between">{{$message->created_at}} <i class="fa-solid fa-comments" style="{{ ($message->answered == 0) ? 'color: red' : 'color: #9fff6b' }}"></i></p>
+                            <h5 class="card-title msg-card-ellipsis">{{$message->apartment->title}}</h5>
+                            <p class="card-text msg-card-ellipsis mb-3">{{$message->content}}</p>
+                            <div class="d-flex justify-content-center align-intems-center">
+                                <a href="{{route('user.messages.edit', $message->id)}}" class="d-inline-block mr-2"><button type="button" class="btn btn-primary">Edit</button></a>                    
+                                <form action="{{route('user.messages.destroy', $message->id)}}" method="POST">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+               </a> 
+                @endif
+            @endforeach
+            <h1 class="container">Messaggi già letti</h1>
+            @foreach ($messages as $message)
+                @if ($message->visualized == 1)
+                <a href="{{route('user.messages.show', $message->id)}}" class="text-decoration-none text-dark">
+                    <div class="card col-12 col-md-4 col-xl-3 m-1 messaggio msg-da-leggere {{ ($message->visualized == 0) ? 'text-white bg-dark' : '' }}" style="width: 20rem !important;">
+                        <img src="{{ asset("storage/{$message->apartment->preview}")}}" class="card-img-top mt-3" alt="...">
+                        <div class="card-body">
+                            <p class="card-text d-flex justify-content-between">{{$message->created_at}} <i class="fa-solid fa-comments" style="{{ ($message->answered == 0) ? 'color: red' : 'color: #9fff6b' }}"></i></p>
+                            <h5 class="card-title msg-card-ellipsis">{{$message->apartment->title}}</h5>
+                            <p class="card-text msg-card-ellipsis mb-3">{{$message->content}}</p>
+                            <div class="d-flex justify-content-center align-intems-center">
+                                <a href="{{route('user.messages.edit', $message->id)}}" class="d-inline-block mr-2"><button type="button" class="btn btn-primary">Edit</button></a>                    
+                                <form action="{{route('user.messages.destroy', $message->id)}}" method="POST">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+               </a> 
+                @endif
+            @endforeach
+        </div>
 </body>
 </html>
