6f58ec75b78eab3848303c3d8d003c22e4fabca2
Masadavide <davide.masadm@gmail.com>
2022-04-22 11:40:57 +0200

    home.blade pronto

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -15,9 +15,86 @@
             </button>
         </a>
     </div>
-    <div class="dashboard row m-4">
-        <div class="col-6 mr-2 p-2 border rounded">
-            <div class="row row-cols-1">
+    <div class="dashboard text-center">
+        <div class="row row-cols-2 row-cols-lg-4 p-2">
+            <div class="col">
+                <button class="border rounded">
+                    <a href="{{route("user.apartments.index")}}">
+                        <div class="row p-3">
+                            <div class="col">
+                                {{-- numero appartamenti totali --}}
+                                10
+                                {{-- {{$apartments->user_id}} --}}
+                                <div>
+                                    Appartamenti
+                                </div>
+                            </div>
+                            <div class="col m-3">
+                                <i class="fas fa-home"></i>
+                            </div>
+                        </div>
+                    </a>
+                </button>
+            </div>
+            <div class="col">
+                <button class="border rounded">
+                    <a href="{{route("user.messages.index")}}">
+                        <div class="row row-cols-sm-12 p-3">
+                            <div class="col">
+                                {{-- numero appartamenti totali --}}
+                                10
+                                {{-- {{$user->messages}} --}}
+                                <div>
+                                    Tutti i messaggi
+                                </div>
+                            </div>
+                            <div class="col m-3">
+                                <i class="fas fa-comment"></i>
+                            </div>
+                        </div>
+                    </a>
+                </button>
+                
+            </div>
+            <div class="col mt-3 mt-lg-0">
+                <button class="border rounded">
+                    <a href="">
+                        <div class="row row-cols-sm-12 p-3">
+                            <div class="col">
+                                {{-- numero messaggi non letti --}}
+                                1
+                                {{-- {{apartment.user_id}} --}}
+                                <div>
+                                    Messaggi non letti
+                                </div>
+                            </div>
+                            <div class="col m-3">
+                                <i class="fas fa-envelope"></i>
+                            </div>
+                        </div>
+                    </a>
+                </button>
+            </div>
+            <div class="col mt-3 mt-lg-0">
+                <button class="border rounded">
+                    <div class="row row-cols-sm-12 p-3">
+                        <div class="col">
+                            {{-- numero messaggi non letti --}}
+                            100
+                            {{-- {{analytics.view}} --}}
+                            <div>
+                                Visualizzazioni Totali
+                            </div>
+                        </div>
+                        <div class="col m-3">
+                            <i class="fas fa-eye"></i>
+                        </div>
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
+            {{-- <div class="row row-cols-1">
                 <div class="col d-flex flex-column flex-sm-row text-center text-sm-left my-3">
                     <span class="dashboard-icon mx-3">
                         <i class="fas fa-comment"></i>
@@ -25,7 +102,7 @@
                     <a href="{{route("user.messages.index")}}" class="">
                         Tutti i messaggi
                     </a>
-                </div>
+                </div> --}}
                 {{-- <div class="col d-flex flex-column flex-sm-row text-center text-sm-left my-3">
                     <span class="mx-3">
                         <i class="fas fa-edit"></i>
@@ -34,64 +111,6 @@
                         Gestisci gli annunci
                     </a>
                 </div> --}}
-            </div>
-        </div>
-        <div class="button col mt-2">
-            <div class="row">
-                <button class="col mr-2 border rounded">
-                    <a href="{{route("user.apartments.index")}}">
-                        <div class="row p-3">
-                            <div class="col text-left">
-                                {{-- numero appartamenti totali --}}
-                                10
-                                {{-- {{$apartments->user_id}} --}}
-                                <div>
-                                    Annunci
-                                </div>
-                            </div>
-                            <div class="icon col position-relative m-3">
-                                <i class="fas fa-home position-absolute"></i>
-                            </div>
-                        </div>
-                    </a>
-                </button>
-                <button class="button-2 col mt-2 mr-2 border rounded">
-                    <a href="">
-                        <div class="row p-3">
-                            <div class="col text-left">
-                                {{-- numero messaggi non letti --}}
-                                1
-                                {{-- {{apartment.user_id}} --}}
-                                <div>
-                                    Messaggi non letti
-                                </div>
-                            </div>
-                            <div class="icon col position-relative m-3">
-                                <i class="fas fa-envelope position-absolute"></i>
-                            </div>
-                        </div>
-                    </a>
-                </button>
-                <button class="button-3 col mt-2 mr-2 border rounded">
-                    <div class="row p-3">
-                        <div class="col text-left">
-                            {{-- numero messaggi non letti --}}
-                            100
-                            {{-- {{analytics.view}} --}}
-                            <div>
-                                Visualizzazioni Totali
-                            </div>
-                        </div>
-                        <div class="icon col position-relative m-3">
-                            <i class="fas fa-eye position-absolute"></i>
-                        </div>
-                    </div>
-                </button>
-            </div>
-        </div>
-    </div>
-        
-        
         {{-- <div class="card-body">
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
