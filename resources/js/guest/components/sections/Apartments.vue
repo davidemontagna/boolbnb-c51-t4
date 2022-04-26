23d478535dbb6452d289f93703f60f688491e4ed
Masadavide <davide.masadm@gmail.com>
2022-04-26 22:23:09 +0200

    modificata vista home

--- resources/js/guest/components/sections/Apartments.vue
+++ resources/js/guest/components/sections/Apartments.vue
@@ -1,87 +1,113 @@
 <template>
     <section>
-        <div class="main-searchbar d-flex justify-content-center align-item-center">
-            <div class="form">
-                <!-- <input type="text" name="text" id="text" placeholder="search" v-model.trim="searchInput"> -->
-                <div class="d-flex search-main-section">
-                    <input class="ms_address" type="text" id="address" name="address" @keyup.prevent="getLocations" placeholder="Cerca località" v-model.trim="searchInput">
-                    <select class="form-control" v-model="selectedLocation">
-                        <option disabled> Seleziona una località </option>
-                        <option :value="index" v-for="(location, index) in setOption" :key="index">{{location.address.freeformAddress + ', ' + location.address.country}}</option>
-                    </select>
-                    <p>
-                        <button class="btn-filtri" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                            <i class="fa-solid fa-filter"></i>
-                        </button>
-                    </p>
-                    <button type="submit" value="Submit" id="submit" class="d-flex justify-content-center align-items-center" @click.prevent="getApartments"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-                <div class="collapse" id="collapseExample">
-                        <div class="card card-body rounded-pill">
-                            <div class=" input-item-container d-flex">
-                            <div class="multiselect" id="multiselect">
-                                <div class="services" @click="services()">Servizi<i class="fa-solid fa-arrow-down"></i></div>
-                                <div class="multiselect-options hidden" id="multiselectOptions">
-                                    <div v-for="service in allServices" :key="service.id">
-                                        <input type="checkbox" :name="service.name" :id="service.name" v-model="CheckedServices" :value="service.name" class="checkboxServices">
-                                        <label for="checkbox1">{{service.name}}</label>
-                                        <i :class="service.icon"></i>
+        <div class="row row-cols-1">
+            <div class="col">
+                <div class="main-searchbar d-flex justify-content-center align-item-center">
+                    <div class="form">
+                        <div class="d-flex search-main-section justify-content-between">
+                            <input class="ms_address" type="text" id="address" name="address" @keyup.prevent="getLocations" placeholder="Cerca" v-model.trim="searchInput">
+                            <select class="form-control" v-model="selectedLocation">
+                                <option disabled> Seleziona una località </option>
+                                <option :value="index" v-for="(location, index) in setOption" :key="index">{{location.address.freeformAddress + ', ' + location.address.country}}</option>
+                            </select>
+                            <p>
+                                <button class="btn-filtri" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fa-solid fa-filter"></i>
+                                </button>
+                            </p>
+                            <button type="submit" value="Submit" id="submit" class="d-flex justify-content-center align-items-center" @click.prevent="getApartments" @click="addClass = !addClass" ><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
+                        <div class="collapse" id="collapseExample">
+                            <div class="card card-body rounded-pill">
+                                <div class="input-item-container d-flex justify-content-center">
+                                    <div class="multiselect" id="multiselect">
+                                        <div class="services" @click="services()">Servizi<i class="fa-solid fa-arrow-down"></i></div>
+                                        <div class="multiselect-options hidden" id="multiselectOptions">
+                                            <div v-for="service in allServices" :key="service.id">
+                                                <input type="checkbox" :name="service.name" :id="service.name" v-model="CheckedServices" :value="service.name" class="checkboxServices">
+                                                <label for="checkbox1">{{service.name}}</label>
+                                                <i :class="service.icon"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="input-item">
+                                        <label for="range" class="range-label"><i class="fa-solid fa-circle-nodes"></i>km: {{inputRange}}</label>
+                                        <input type="range" name="range" id="range" step="20" v-model="inputRange"  min="20" max="200" class="">
+                                    </div>
+                                    <div class="input-item ms_input-item">
+                                        <label for="beds" class="">Letti:</label>
+                                        <input type="number" name="beds" id="beds" v-model="inputBeds"  min="1" max="10" class="">
+                                    </div>
+                                    <div class="input-item ms_input-item">
+                                        <label for="rooms" class="">Stanze:</label>
+                                        <input type="number" name="rooms" id="rooms" v-model="inputRooms"  min="1" max="10" class="">
+                                    </div>
+                                    <div class="input-item ms_input-item">
+                                        <label for="bath" class="">Bagni:</label>
+                                        <input type="number" name="bath" id="bath" v-model="inputBath"  min="1" max="10" class="">
                                     </div>
                                 </div>
                             </div>
-                            <div class="input-item">
-                                <label for="range" class="range-label"><i class="fa-solid fa-circle-nodes"></i>km: {{inputRange}}</label>
-                                <input type="range" name="range" id="range" step="20" v-model="inputRange"  min="20" max="200" class="">
-                            </div>
-                            <div class="input-item">
-                                <label for="beds" class=""><i class="fa-solid fa-bed"></i></label>
-                                <input type="number" name="beds" id="beds" v-model="inputBeds"  min="1" max="10" class="">
-                            </div>
-                            <div class="input-item">
-                                <label for="rooms" class="">Rooms:</label>
-                                <input type="number" name="rooms" id="rooms" v-model="inputRooms"  min="1" max="10" class="">
-                            </div>
-                            <div class="input-item">
-                                <label for="bath" class=""><i class="fa-solid fa-shower"></i></label>
-                                <input type="number" name="bath" id="bath" v-model="inputBath"  min="1" max="10" class="">
-                            </div>
-
-                            <!-- <button type="submit" value="Search" id="submit" @click.prevent="search"><i class="fa-solid fa-magnifying-glass"></i></button> -->
-                            </div>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
 
 
         
-        <div class="ms_apartment">
+        <div class="ms_apartment" id="apartment" :class="{ ms_active: addClass }">
             <h2>Le nostre strutture</h2>
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mx-auto p-3">
                 <div class="col my-3" v-for="(apartment, index) in setApartments" :key="index">
                     <router-link :to="{name: 'single-apartment', params:{slug: apartment.slug}}" class="router-link">
-                    <div class="ms_card" :class="checkSponsorized(apartment) ? 'sponsorized-card' : ''">
-                        <div class="sponzorized-tag" :class="checkSponsorized(apartment) ? '' : 'hidden'">Uno dei nostri preferiti <i class="fa-solid fa-crown"></i></div>
-                        <div class="ms_img mx-auto position-relative">
-                                <img :src="'../storage/'+apartment.preview" alt="">
-                                <div class="ms_shadow position-absolute"></div>
-                            <div class="ms_description position-absolute">
-                                <p class="ms_beds mx-2">Numero di letti: {{apartment.num_beds}}</p>
+                        <div class="ms_card" :class="checkSponsorized(apartment) ? 'sponsorized-card' : ''">
+                            <div class="sponzorized-tag" :class="checkSponsorized(apartment) ? '' : 'hidden'">Uno dei nostri preferiti <i class="fa-solid fa-crown"></i></div>
+                            <div class="ms_img mx-auto position-relative">
+                                    <img :src="'../storage/'+apartment.preview" alt="">
+                                    <div class="ms_shadow position-absolute"></div>
+                                <div class="ms_description position-absolute">
+                                    <p class="ms_beds mx-2">Numero di letti: {{apartment.num_beds}}</p>
+                                </div>
+                            </div>
+                            <div class="ms_text">
+                                <h2 class="ms_title mt-3">{{apartment.title}}</h2>
+                                <h4 class="ms_city mt-3">{{apartment.location.city}}</h4>
+                                <p class="ms_address">{{apartment.location.address}}</p>
+                                <div class="ms_description2 d-flex justify-content-between mt-3">
+                                    <p class="ms_rooms">Stanze: {{apartment.num_rooms}}</p>
+                                    <p class="ms_bath">Bagni: {{apartment.num_bath}}</p>
+                                    <p class="ms_square">Mq: {{apartment.square_footage}}</p>
+                                </div>
+                                <p v-if="checkSponsorized(apartment)" class="sponsorized-text">Post sponsorizzato</p>
                             </div>
                         </div>
-                        <div class="ms_text">
-                            <h2 class="ms_title mt-3">{{apartment.title}}</h2>
-                            <h4 class="ms_city mt-3">{{apartment.location.city}}</h4>
-                            <p class="ms_address">{{apartment.location.address}}</p>
-                            <div class="ms_description2 d-flex justify-content-between mt-3">
-                                <p class="ms_rooms">Stanze: {{apartment.num_rooms}}</p>
-                                <p class="ms_bath">Bagni: {{apartment.num_bath}}</p>
-                                <p class="ms_square">Mq: {{apartment.square_footage}}</p>
-                            </div>
-                            <p v-if="checkSponsorized(apartment)" class="sponsorized-text">Post sponsorizzato</p>
-                        </div>
-                    </div>
                     </router-link>
+                </div>
+            </div>
+        </div>
+        <div class="ms_information">
+            <div class="row row-cols-1 row-cols-lg-3 p-3">
+                <div class="col my-3 t-center">
+                    <div class="ms_img_information">
+                        <img :src="require('/storage/app/public/resorces/img-1.svg')" alt="">
+                    </div>
+                    <h4 class="mt-2">Cerca velocemente, cerca in modo intelligente</h4>
+                    <p class="mt-2">Sfoglia centinaia di alloggi nella tua città preferita. Così da non perdere il posto dei tuoi sogni!</p>
+                </div>
+                <div class="col my-3">
+                    <div class="ms_img_information">
+                        <img :src="require('/storage/app/public/resorces/img-3.svg')" alt="">
+                    </div>
+                    <h4 class="mt-2">Confronta i vari servizi per trovare l'aloggio che fà per te</h4>
+                    <p class="mt-2">Non farti trovare impreparato dopo una pioggierella estiva, assicurati che l'asciugacapelli sia presente in struttura</p>
+                </div>
+                <div class="col my-3">
+                    <div class="ms_img_information">
+                        <img :src="require('/storage/app/public/resorces/img-2.svg')" alt="">
+                    </div>
+                    <h4 class="mt-2">Chatta con persone reali</h4>
+                    <p class="mt-2">Comunica direttamente con gli inserzionisti. Per verificare la disponibilità o per richiedere servizi aggiuntivi</p>
                 </div>
             </div>
         </div>
@@ -107,6 +133,7 @@ export default {
             selectedLocation: 0,
             locationList: [],
             apiKey: 'LmxBM8DrAJjBA1BQPufxlrTGrO4c4Byh',
+            addClass: false,
         }
     },
     methods: {
@@ -258,6 +285,9 @@ export default {
 @import '../../../../sass/variables.scss';
 
 section{
+    .ms_apartment{
+        display: none;
+    }
     .ms_card{
         width: 100%;
         height: 500px;
@@ -440,6 +470,12 @@ section{
         align-items: center;
     }
 }
+.ms_input-item{
+    width: 110px;
+    input{
+        width: 45px;
+    }
+}
 
 
 .multiselect-options{
@@ -448,16 +484,18 @@ section{
     background-color: white;
     overflow: hidden;
     overflow-y: auto ;
-    height: 100px;
+    height: 200px;
     width: 100%;
     left: 0;
     padding: 1rem;
     border: 1px solid #c2c2c2;
+    border-radius: 10px;
+    z-index: 2;
 }
 .hidden{
     display: none;
 }
-@media (max-width: 768px) {
+@media (max-width: 992px) {
     .main-searchbar{
     position: relative;
     top: -30px;
@@ -499,8 +537,8 @@ section{
         align-items: center;
         justify-content: center;
     }
-     .btn-filtri:active, .btn-filtri:focus{
-         box-shadow: inset 0 0 5px 1px rgba(0, 0, 0, .4);
+    .btn-filtri:active, .btn-filtri:focus{
+        box-shadow: inset 0 0 5px 1px rgba(0, 0, 0, .4);
         font-size: 1.2rem;
     }
     .search-main-section{
@@ -523,7 +561,6 @@ section{
     .collapse{
         .input-item-container{
         flex-wrap: wrap !important;
-        width: 400px !important;
         justify-content: space-between;
         align-items: center;
         }
@@ -549,6 +586,9 @@ section{
         align-items: center;
     }
 }
+.card-body{
+    border-radius: 40px!important;
+}
 }
 
 //sponzorized
@@ -563,7 +603,7 @@ section{
     top: 2rem;
     left: 3rem;
     background-color: $primary-red;
-    z-index: 3;
+    z-index: 1;
     color: white;
     padding: 5px;
     border-radius: 5px;
@@ -580,5 +620,16 @@ section{
 }
 .ms_address{
     margin-bottom: -3px!important;
+}
+.ms_active{
+    display: block!important;
+}
+.ms_img_information{
+    width: 150px;
+    height: 100px;
+    img{
+        width: 100%;
+        height: 100%;
+    }
 }
 </style>
